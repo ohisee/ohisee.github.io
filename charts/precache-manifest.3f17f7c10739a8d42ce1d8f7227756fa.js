b0278c2076179030c25d27980e2f0057a9641cc0
ohisee <breakosin@hotmail.com>
2019-07-22 22:21:26 -0700

    React app

---
+++ charts/precache-manifest.3f17f7c10739a8d42ce1d8f7227756fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52d0d3d22437f4c9554a2e5d7471057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d9a619d38f799381db",
+    "url": "/static/css/main.b3b8ea78.chunk.css"
+  },
+  {
+    "revision": "8e2a2044d7f456bbe436",
+    "url": "/static/js/2.6eaa6143.chunk.js"
+  },
+  {
+    "revision": "20d9a619d38f799381db",
+    "url": "/static/js/main.7a5ac13d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
