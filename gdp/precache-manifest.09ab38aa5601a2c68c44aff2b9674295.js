b725d921f06cc1adc17c6aed15268da5b46ba1a3
ohisee <breakosin@hotmail.com>
2019-07-22 22:39:29 -0700

    New React app

---
+++ gdp/precache-manifest.09ab38aa5601a2c68c44aff2b9674295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8a946b86e0de1bd97239cca499f522",
+    "url": "/gdp/index.html"
+  },
+  {
+    "revision": "20d9a619d38f799381db",
+    "url": "/gdp/static/css/main.b3b8ea78.chunk.css"
+  },
+  {
+    "revision": "8e2a2044d7f456bbe436",
+    "url": "/gdp/static/js/2.6eaa6143.chunk.js"
+  },
+  {
+    "revision": "20d9a619d38f799381db",
+    "url": "/gdp/static/js/main.7a5ac13d.chunk.js"
+  },
+  {
+    "revision": "a38be8a014dec29134a7",
+    "url": "/gdp/static/js/runtime~main.a4e2f29e.js"
+  }
+]);
