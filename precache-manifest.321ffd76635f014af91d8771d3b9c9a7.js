266570b234babeb3e40a055e24936f6ef7575bd3
ohisee <breakosin@hotmail.com>
2019-07-27 19:27:23 -0700

    New React app

---
+++ precache-manifest.321ffd76635f014af91d8771d3b9c9a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e79c32ce681796e113d210ba0c3f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "378299251e1acdfe746f",
+    "url": "/static/css/main.6becd8dd.chunk.css"
+  },
+  {
+    "revision": "d3f093400067821bf360",
+    "url": "/static/js/2.1cebdb7b.chunk.js"
+  },
+  {
+    "revision": "378299251e1acdfe746f",
+    "url": "/static/js/main.5d16f84b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
