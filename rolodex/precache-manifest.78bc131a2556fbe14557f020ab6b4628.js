5ea4cfa7d0a5fa20f1a91dacbc813a2e24da3cfd
ohisee <breakosin@hotmail.com>
2020-09-03 00:30:10 -0700

    continued in development

---
+++ rolodex/precache-manifest.78bc131a2556fbe14557f020ab6b4628.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125980f50ce62945a6697f622ad080c4",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "fedd542cfa3927abad23",
+    "url": "/rolodex/static/css/main.8457e9a4.chunk.css"
+  },
+  {
+    "revision": "ac94a80897d790f2121b",
+    "url": "/rolodex/static/js/2.215c97a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.215c97a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedd542cfa3927abad23",
+    "url": "/rolodex/static/js/main.1bb5bd44.chunk.js"
+  },
+  {
+    "revision": "08f89c8e353e98eb57af",
+    "url": "/rolodex/static/js/runtime-main.e0da3420.js"
+  },
+  {
+    "revision": "0edc75481b7357da323f8c7f0ef3f1a9",
+    "url": "/rolodex/static/media/1.0edc7548.png"
+  },
+  {
+    "revision": "ebf120cf00d34cf7acd14d89628875c8",
+    "url": "/rolodex/static/media/10.ebf120cf.png"
+  },
+  {
+    "revision": "ce05a34166d8ac54865f443cc1cf189d",
+    "url": "/rolodex/static/media/2.ce05a341.png"
+  },
+  {
+    "revision": "d5cd9590c96faa8c2932ff1c557a6596",
+    "url": "/rolodex/static/media/3.d5cd9590.png"
+  },
+  {
+    "revision": "64a79987d98fe22172209d4bb2d36af7",
+    "url": "/rolodex/static/media/4.64a79987.png"
+  },
+  {
+    "revision": "3c59272298af39a7428c3efab3e4ec0b",
+    "url": "/rolodex/static/media/5.3c592722.png"
+  },
+  {
+    "revision": "9edd14c82b9f7d8e83983198a0df349c",
+    "url": "/rolodex/static/media/6.9edd14c8.png"
+  },
+  {
+    "revision": "ab4bb55e8a994989d3011d79ed072904",
+    "url": "/rolodex/static/media/7.ab4bb55e.png"
+  },
+  {
+    "revision": "308fe3178754aa2ab6c5796f691a4d62",
+    "url": "/rolodex/static/media/8.308fe317.png"
+  },
+  {
+    "revision": "f9da866fe440b80826edd4e6e581b100",
+    "url": "/rolodex/static/media/9.f9da866f.png"
+  }
+]);
